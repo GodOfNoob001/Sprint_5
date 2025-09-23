35cbb904a0d7df86d319ecaf1b3a934e15e9bad0
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:37:52 +0300

    Все методы внесены в тестовый класс, убраны тестовые данные из параметризации, добавлена ссылка на файл с параметрами, убраны тестовые данные из теста, добавлена ссылка на файл data, убрана ссылка на сайт, добавлена ссылка на файл urls

--- tests/test_register.py
+++ tests/test_register.py
@@ -6,17 +6,18 @@ import random
 from locators import Locators
 import pytest
 
-@pytest.mark.parametrize('password', ['123456', '123456P', '123456PRAKTIKUM'] )
-def test_happy_path_register_with_valid_values_of_name_email_password(password):
-    driver = webdriver.Chrome()
-    driver.get('https://stellarburgers.nomoreparties.site/register')
-    driver.find_element(*Locators.NAME_INPUT_LOCATOR).send_keys("Александр")
-    driver.find_element(*Locators.EMAIL_INPUT_LOCATOR).send_keys(f"alexdmiterko30{random.randint(100, 999)}@ya.ru")
-    driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(password)
-    driver.find_element(*Locators.REGISTER_BUTTON).click()
-    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
-    assert driver.current_url == 'https://stellarburgers.nomoreparties.site/login'
-    driver.quit()
+class TestStellarsBurgersRegister:
+    @pytest.mark.parametrize('password', Data.valid_passwords )
+    def test_happy_path_register_with_valid_values_of_name_email_password(self, password):
+        driver = webdriver.Chrome()
+        driver.get(URLS.register_url)
+        driver.find_element(*Locators.NAME_INPUT_LOCATOR).send_keys(Data.valid_name)
+        driver.find_element(*Locators.EMAIL_INPUT_LOCATOR).send_keys(Data.random_valid_mail)
+        driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(password)
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
+        assert driver.current_url == URLS.login_url
+        driver.quit()
 
 def test_invalid_path_register_with_empty_name_field():
     driver = webdriver.Chrome()
