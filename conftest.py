cf3e54a1a7b7165fa0a565a486adae8acd3b0c3f
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:24:03 +0300

    убраны данные, внесён импорт данных из файла data

--- conftest.py
+++ conftest.py
@@ -11,8 +11,8 @@ def open_session():
     driver.get(URLS.base_url)
     driver.find_element(*Locators.ENTER_BUTTON_ON_MAIN_PAGE).click()
     WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
-    driver.find_element(*Locators.EMAIL_LOGIN).send_keys("alexdmiterko301212@ya.ru")
-    driver.find_element(*Locators.PASSWORD_LOGIN).send_keys("YaPraktikum123")
+    driver.find_element(*Locators.EMAIL_LOGIN).send_keys(Data.valid_login)
+    driver.find_element(*Locators.PASSWORD_LOGIN).send_keys(Data.valid_password)
     driver.find_element(*Locators.ENTER_BUTTON).click()
     WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
     yield driver
