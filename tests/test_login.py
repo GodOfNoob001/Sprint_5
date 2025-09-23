fcbc12ac09b69e4df6febfd35cad1634c64e5008
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:11:47 +0300

    убрана прямая ссылка, добавлена ссылка на файл с собраными urls

--- tests/test_login.py
+++ tests/test_login.py
@@ -44,15 +44,15 @@ class TestStellarsBurgerLogin:
         driver.quit()
 
 
-def test_happy_path_success_login_from_forgot_password_page_with_valid_values():
-    driver = webdriver.Chrome()
-    driver.get('https://stellarburgers.nomoreparties.site/forgot-password')
-    driver.find_element(*Locators.LOGIN_LINK_BUTTON).click()
-    WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
-    driver.find_element(*Locators.EMAIL_LOGIN).send_keys("alexdmiterko301212@ya.ru")
-    driver.find_element(*Locators.PASSWORD_LOGIN).send_keys("YaPraktikum123")
-    driver.find_element(*Locators.ENTER_BUTTON).click()
-    WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
-    assert driver.find_element(*Locators.MAKE_AN_ORDER_BUTTON).is_displayed()
-    driver.quit()
+    def test_happy_path_success_login_from_forgot_password_page_with_valid_values(self):
+        driver = webdriver.Chrome()
+        driver.get(URLS.forgot_password_url)
+        driver.find_element(*Locators.LOGIN_LINK_BUTTON).click()
+        WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
+        driver.find_element(*Locators.EMAIL_LOGIN).send_keys("alexdmiterko301212@ya.ru")
+        driver.find_element(*Locators.PASSWORD_LOGIN).send_keys("YaPraktikum123")
+        driver.find_element(*Locators.ENTER_BUTTON).click()
+        WebDriverWait(driver, 10).until(expected_conditions.presence_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
+        assert driver.find_element(*Locators.MAKE_AN_ORDER_BUTTON).is_displayed()
+        driver.quit()
 
