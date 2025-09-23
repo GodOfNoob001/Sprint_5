a914a4aa1e9ed2f1599921c708e3d6adf5a7f886
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 19:32:02 +0300

    Функции внесены в соответствующий тестовый класс

--- tests/test_transitions.py
+++ tests/test_transitions.py
@@ -4,10 +4,11 @@ from selenium.webdriver.support.wait import WebDriverWait
 from locators import Locators
 from urls import URLS
 
-def test_happy_path_success_transition_from_main_page_to_personal_account(open_session):
-    open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
-    WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
-    assert open_session.current_url == URLS.profile_url
+class TestStellarsBurgersTransitions:
+    def test_happy_path_success_transition_from_main_page_to_personal_account(self, open_session):
+        open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
+        WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
+        assert open_session.current_url == URLS.profile_url
 
     def test_happy_path_success_transition_from_personal_account_to_constructor_by_button(self, open_session):
         open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
