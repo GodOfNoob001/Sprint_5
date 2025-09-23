1c20bcfc659ec0d64eac1c34677d4f173a52b09d
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:39:09 +0300

    убраны тестовые данные, внесены ссылки на файлы с соответствующими данными

--- tests/test_register.py
+++ tests/test_register.py
@@ -40,14 +40,14 @@ def test_invalid_path_register_with_invalid_email_no_domain():
     assert driver.current_url != 'https://stellarburgers.nomoreparties.site/login'
     driver.quit()
 
-@pytest.mark.parametrize('password', ['1', '12', '123', '1234', '12345'])
-def test_invalid_path_register_with_invalid_password_no_6_symbols(password):
-    driver = webdriver.Chrome()
-    driver.get('https://stellarburgers.nomoreparties.site/register')
-    driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(password)
-    driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(Keys.TAB)
-    error_element = WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ERROR_FIELD))
-    error_text = driver.find_element(*Locators.ERROR_MESSAGE).text
-    assert error_element.is_displayed()
-    assert error_text == "Некорректный пароль"
-    driver.quit()
+    @pytest.mark.parametrize('password', Data.invalid_passwords)
+    def test_invalid_path_register_with_invalid_password_no_6_symbols(self, password):
+        driver = webdriver.Chrome()
+        driver.get(URLS.register_url)
+        driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(password)
+        driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(Keys.TAB)
+        error_element = WebDriverWait(driver, 3).until(expected_conditions.visibility_of_element_located(Locators.ERROR_FIELD))
+        error_text = driver.find_element(*Locators.ERROR_MESSAGE).text
+        assert error_element.is_displayed()
+        assert error_text == Data.error_text
+        driver.quit()
