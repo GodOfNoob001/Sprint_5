d033a8bb3d4dc7ee900337bbcb2df953ba1cb6d9
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:12:52 +0300

    добавлен тестовый класс

--- tests/test_logout.py
+++ tests/test_logout.py
@@ -3,9 +3,10 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 from locators import Locators
 
-def test_happy_path_success_logout(open_session):
-    open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
-    WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
-    open_session.find_element(*Locators.EXIT_BUTTON).click()
-    WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
-    assert open_session.current_url == 'https://stellarburgers.nomoreparties.site/login'
+class TestStellarsBurgersLogout:
+    def test_happy_path_success_logout(self, open_session):
+        open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
+        WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
+        open_session.find_element(*Locators.EXIT_BUTTON).click()
+        WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.ENTER_BUTTON))
+        assert open_session.current_url == 'https://stellarburgers.nomoreparties.site/login'
