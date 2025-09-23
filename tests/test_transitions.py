52d187ddfc699bf810676094c55df291d82f1f91
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 19:31:55 +0300

    Функции внесены в соответствующий тестовый класс

--- tests/test_transitions.py
+++ tests/test_transitions.py
@@ -16,9 +16,9 @@ def test_happy_path_success_transition_from_personal_account_to_constructor_by_b
     WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
     assert open_session.current_url == URLS.base_url
 
-def test_happy_path_success_transition_from_personal_account_to_constructor_by_logo(open_session):
-    open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
-    WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
-    open_session.find_element(*Locators.LOGO).click()
-    WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
-    assert open_session.current_url == URLS.base_url
+    def test_happy_path_success_transition_from_personal_account_to_constructor_by_logo(self, open_session):
+        open_session.find_element(*Locators.PERSONAL_ACCOUNT_BUTTON).click()
+        WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.EXIT_BUTTON))
+        open_session.find_element(*Locators.LOGO).click()
+        WebDriverWait(open_session, 3).until(expected_conditions.visibility_of_element_located(Locators.MAKE_AN_ORDER_BUTTON))
+        assert open_session.current_url == URLS.base_url
