7265516bdb13594a0719f582ebc0a77b31d6226b
Alex Dmiterko <dmiterko.sascha5@gmail.com>
2025-09-23 18:38:10 +0300

    убраны тестовые данные, добавлены ссылки на соответствующие файлы с данными

--- tests/test_register.py
+++ tests/test_register.py
@@ -19,14 +19,14 @@ class TestStellarsBurgersRegister:
         assert driver.current_url == URLS.login_url
         driver.quit()
 
-def test_invalid_path_register_with_empty_name_field():
-    driver = webdriver.Chrome()
-    driver.get('https://stellarburgers.nomoreparties.site/register')
-    driver.find_element(*Locators.EMAIL_INPUT_LOCATOR).send_keys(f"alexdmiterko30{random.randint(100, 999)}@ya.ru")
-    driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys("123456yapraktikum")
-    driver.find_element(*Locators.REGISTER_BUTTON).click()
-    assert driver.current_url != 'https://stellarburgers.nomoreparties.site/login'
-    driver.quit()
+    def test_invalid_path_register_with_empty_name_field(self):
+        driver = webdriver.Chrome()
+        driver.get(URLS.register_url)
+        driver.find_element(*Locators.EMAIL_INPUT_LOCATOR).send_keys(Data.random_valid_mail)
+        driver.find_element(*Locators.PASSWORD_INPUT_LOCATOR).send_keys(Data.valid_password)
+        driver.find_element(*Locators.REGISTER_BUTTON).click()
+        assert driver.current_url != URLS.login_url
+        driver.quit()
 
 def test_invalid_path_register_with_invalid_email_no_domain():
     driver = webdriver.Chrome()
